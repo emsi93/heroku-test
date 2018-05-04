28d6e24cb3844c75109357f2b80d68bf3d8ef4f7
emsi93 <krejpowiczmichal@gmail.com>
2018-05-04 13:23:17 +0200

    first commit

---
+++ src/main/java/com/example/demo/config/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping( value = "/index", method = RequestMethod.GET)
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+}
